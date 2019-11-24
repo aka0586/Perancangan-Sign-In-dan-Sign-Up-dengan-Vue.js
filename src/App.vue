51e22e96f3d3e151035dac3e2c1755d9e61ed972
Baangbrow <akhmad.0586@students.amikom.ac.id>
2019-11-24 09:52:04 +0700

    Tambah framework App.vue
    yang terdapat Logger.js , main.js , router.js dan store.js

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <body id="app">
+    <router-view/>
+  </body>
+</template>
+<script></script>
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
